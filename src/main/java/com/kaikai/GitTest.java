f47e05fa765ed293832e7fbdccf367a6e70efcf9
kai <920101@qq.com>
2021-07-26 17:27:08 +0800

    master 2 commit

--- src/main/java/com/kaikai/GitTest.java
+++ src/main/java/com/kaikai/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello git 3");
         System.out.println("hello git 4");
+        System.out.println("hello gitww 4");
     }
 }
