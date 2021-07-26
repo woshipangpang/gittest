13d3e3ad26310e9544bab9df72e453390f3cdc24
kai <920101@qq.com>
2021-07-26 17:08:09 +0800

    third commit

--- src/main/java/com/kaikai/GitTest.java
+++ src/main/java/com/kaikai/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("我叫凯凯");
         System.out.println("hello");
+        System.out.println("hello git 3");
     }
 }
